6964292d58f7c70cb51df80a941d210dd1e97eac
Tony Hillerson <tony.hillerson@effectiveui.com>
2009-12-21 12:28:10 -0700

    initial project creation

---
+++ Classes/LocationsAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  LocationsAppDelegate.h
+//  Locations
+//
+//  Created by Tony Hillerson on 12/21/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+@interface LocationsAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
